28e286187e9145da4f107ec66f7b0a379d3dc02d
Viktor Gavrilenko <viktargavrilenko@gmail.com>
2022-02-22 09:13:45 +0300

    Corrected type of variables

--- src/main/java/databasequeries/GomelRwQueries.java
+++ src/main/java/databasequeries/GomelRwQueries.java
@@ -192,7 +192,7 @@ public class GomelRwQueries {
                         resultSet.getInt(TableNumberPeopleEnterprises.id_pred.toString()),
                         resultSet.getInt(number.toString())));
                 Logger.getInstance().info(String.format("%s - %s",
-                        resultSet.getString(number.toString()), resultSet.getInt(namepred.toString())));
+                        resultSet.getInt(number.toString()), resultSet.getString(namepred.toString())));
             }
 
         } catch (SQLException e) {
